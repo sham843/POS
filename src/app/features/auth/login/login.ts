d9612667b6421179600603bde2d03b85cc6605e1
sham843 <sham.jadhav@shauryatechnosoft.com>
2026-06-11 11:29:09 +0530

    feat: implement user authentication login feature with reactive forms and global styling

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,30 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login { }
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login payload:', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
