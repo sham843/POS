b1ba98cc0b96b44845d9c61483bbac8c2f396cdf
sham843 <sham.jadhav@shauryatechnosoft.com>
2026-06-11 17:53:03 +0530

    feat: remove unused components and styles from counter-sale feature

--- src/app/features/counter-sale/counter-sale.ts
+++ src/app/features/counter-sale/counter-sale.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ProductList } from './components/product-list/product-list';
-import { Cart } from './components/cart/cart';
-import { BillSummary } from './components/bill-summary/bill-summary';
-import { Payment } from './components/payment/payment';
+
 
 @Component({
   selector: 'app-counter-sale',
    standalone: true,
-  imports: [CommonModule, ProductList, Cart, BillSummary, Payment],
+  imports: [CommonModule],
   templateUrl: './counter-sale.html',
   styleUrl: './counter-sale.scss',
 })
