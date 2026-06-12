09fb228ea5e13dff0204dc2ad2e202a149cafd86
sham843 <sham.jadhav@shauryatechnosoft.com>
2026-06-12 10:09:34 +0530

    feat: implement initial layout and component structure for counter sale feature

--- src/app/features/counter-sale/counter-sale.ts
+++ src/app/features/counter-sale/counter-sale.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { ProductList } from './components/product-list/product-list';
+import { Cart } from './components/cart/cart';
+import { BillSummary } from './components/bill-summary/bill-summary';
+import { Payment } from './components/payment/payment';
 
 @Component({
   selector: 'app-counter-sale',
-   standalone: true,
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule,
+    ProductList,
+    Cart,
+    BillSummary,
+    Payment
+  ],
   templateUrl: './counter-sale.html',
   styleUrl: './counter-sale.scss',
 })
