a57b1e4dbb377e89bd15008dd009d249f7a09230
sham843 <sham.jadhav@shauryatechnosoft.com>
2026-06-12 16:34:48 +0530

    feat: implement authGuard and integrate route protection for protected application modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login) },
-  { path: 'session-summary', loadComponent: () => import('./features/session-summary/session-summary').then(m => m.SessionSummary) },
-  { path: 'counter-sale', loadComponent: () => import('./features/counter-sale/counter-sale').then(m => m.CounterSale) }
+  { path: 'session-summary', canActivate: [authGuard], loadComponent: () => import('./features/session-summary/session-summary').then(m => m.SessionSummary) },
+  { path: 'counter-sale', canActivate: [authGuard], loadComponent: () => import('./features/counter-sale/counter-sale').then(m => m.CounterSale) }
 ];
