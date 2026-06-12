af64a1d93cd89c376a3b3fa2531972073920797c
sham843 <sham.jadhav@shauryatechnosoft.com>
2026-06-12 16:59:06 +0530

    feat: implement 404 not-found page and add wildcard route navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login) },
   { path: 'session-summary', canActivate: [authGuard], loadComponent: () => import('./features/session-summary/session-summary').then(m => m.SessionSummary) },
-  { path: 'counter-sale', canActivate: [authGuard], loadComponent: () => import('./features/counter-sale/counter-sale').then(m => m.CounterSale) }
+  { path: 'counter-sale', canActivate: [authGuard], loadComponent: () => import('./features/counter-sale/counter-sale').then(m => m.CounterSale) },
+  { path: '**', loadComponent: () => import('./features/not-found/not-found.component').then(m => m.NotFoundComponent) }
 ];
