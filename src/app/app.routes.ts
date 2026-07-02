d2be048a2c7e86f398ebc77cb4b280313da1b135
sham843 <sham.jadhav@shauryatechnosoft.com>
2026-07-02 18:19:14 +0530

    feat: implement main application layout and configure lazy-loaded routing for dashboard, sales, reports, and settings.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,9 +13,19 @@ export const routes: Routes = [
     canActivate: [authGuard],
     loadComponent: () => import('./layout/main-layout.component').then(m => m.MainLayoutComponent),
     children: [
-      { path: 'counter-sale', loadComponent: () => import('./features/counter-sale/counter-sale').then(m => m.CounterSale) },
       { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard) },
-      { path: 'reports', loadComponent: () => import('./features/reports/reports').then(m => m.Reports) },
+      { path: 'counter-sale', loadComponent: () => import('./features/counter-sale/counter-sale').then(m => m.CounterSale) },
+
+      { path: 'reports/user-report', loadComponent: () => import('./features/reports/user-report/user-report').then(m => m.UserReport) },
+      { path: 'reports/product-report', loadComponent: () => import('./features/reports/product-report/product-report').then(m => m.ProductReport) },
+      { path: 'reports/bill-report', loadComponent: () => import('./features/reports/bill-report/bill-report').then(m => m.BillReport) },
+      { path: 'reports/customer-report', loadComponent: () => import('./features/reports/customer-report/customer-report').then(m => m.CustomerReport) },
+      { path: 'reports/all-party-balance', loadComponent: () => import('./features/reports/all-party-balance/all-party-balance').then(m => m.AllPartyBalance) },
+      { path: 'reports/gst-report', loadComponent: () => import('./features/reports/gst-report/gst-report').then(m => m.GstReport) },
+      { path: 'reports/customer-payment-history', loadComponent: () => import('./features/reports/customer-payment-history/customer-payment-history').then(m => m.CustomerPaymentHistory) },
+      { path: 'reports/cash-report', loadComponent: () => import('./features/reports/cash-report/cash-report').then(m => m.CashReport) },
+      { path: 'reports/sales-invoice-report', loadComponent: () => import('./features/reports/sales-invoice-report/sales-invoice-report').then(m => m.SalesInvoiceReport) },
+
       { path: 'settings', loadComponent: () => import('./features/settings/settings').then(m => m.Settings) }
     ]
   },
